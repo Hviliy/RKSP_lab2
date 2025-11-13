08ac0fac5ff02131e7c1c6a695fff06bc545a7b8
Danila <danilakopylov603@gmail.com>
2025-11-13 18:54:15 +0800

    Этап 4

--- src/main/java/ru/kopylov/data_service/controller/StudentController.java
+++ src/main/java/ru/kopylov/data_service/controller/StudentController.java
@@ -1,4 +1,38 @@
 package ru.kopylov.data_service.controller;
 
-public class StudentController {
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import ru.kopylov.data.api.StudentDataApi;
+import ru.kopylov.data.model.StudentDataCreateRequest;
+import ru.kopylov.data.model.StudentDataResponse;
+import ru.kopylov.data_service.data.Student;
+import ru.kopylov.data_service.data.StudentRepository;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentController implements StudentDataApi {
+
+    private final StudentRepository studentRepository;
+
+    @Override
+    public ResponseEntity<StudentDataResponse> createStudentDataInData(StudentDataCreateRequest request) {
+        Student student = new Student();
+        student.setName(request.getFullName());
+        student.setPassport(request.getPassport());
+        studentRepository.save(student);
+        StudentDataResponse response = new StudentDataResponse();
+        response.setId(student.getId());
+        response.setFullName(student.getName());
+        response.setPassport(student.getPassport());
+
+        return ResponseEntity.status(200).body(response);
+    }
+
+  /*@Override
+  public ResponseEntity<StudentDataResponse> getStudentDataByIdFromData(Long id) {
+
+    return ResponseEntity.status(200).body(response);
+  }*/
 }
